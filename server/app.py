1594fd60f83f5d498a5687e4bcb73ac85d14f54a
shakshu-r <shakshu02@gmail.com>
2026-04-08 22:39:54 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -2,8 +2,9 @@ import threading
 from inference import app, run_inference
 
 def main():
-    t = threading.Thread(target=run_inference, daemon=True)
+    t = threading.Thread(target=run_inference, daemon=False)
     t.start()
+    t.join()
 
 if __name__ == "__main__":
     main()
